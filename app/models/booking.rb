76d2273b9d45f133f208e0845c7306b35d32a597
Hernan Montero <hernanm842@gmail.com>
2025-04-08 15:23:46 -0400

    controlador de places y sus vistas

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,10 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :place
   has_many :messages, dependent: :destroy
+
+  enum status: {
+    pending: 0,
+    approved: 1,
+    rejected: 2
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_07_224340) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_08_183259) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,7 +57,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_07_224340) do
     t.float "price"
     t.string "title"
     t.text "description"
-    t.string "type"
+    t.string "place_type"
     t.date "available_date"
     t.float "latitude"
     t.float "longitude"
