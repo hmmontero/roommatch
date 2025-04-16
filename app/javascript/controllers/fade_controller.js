7cc57cf6164201404c12885ddd4911bc69d163fa
piero elguedas <piero2086@gmail.com>
2025-04-16 08:13:02 -0500

    ajustes front

--- app/javascript/controllers/fade_controller.js
+++ app/javascript/controllers/fade_controller.js
@@ -2,7 +2,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = {
+    direction: String
+  }
+
   connect() {
+    const direction = this.directionValue || "up"
+    this.element.classList.add(`fade-${direction}`)
+
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -18,3 +25,4 @@ export default class extends Controller {
     observer.observe(this.element)
   }
 }
+
